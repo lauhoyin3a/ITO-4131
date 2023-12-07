96a987eb5bac6499ec5e87f9f18e6669360c7f9f
lauhoyin3a <62736148+lauhoyin3a@users.noreply.github.com>
2023-12-07 11:49:10 +0800

    Update

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -2,17 +2,33 @@ import java.util.Random;
 
 public class RandomGenerator {
 
-    private Random rand;
+  private static Random rand;
 
-    public RandomGenerator() {
-        rand = new Random();
+  public RandomGenerator() {
+    rand = new Random();
+  }
+
+  public static boolean coinFlip(){
+    int coin = RandomGenerator.generateNumberBetween(0,1);
+    if (coin == 0){
+      return true;
     }
+    return false;
+  }
 
-    public int generateNumberBetween(int lowerBound, int upperBound) {
+  public static int generateNumberBetween(int lowerBound, int upperBound) {
 
-        int randomNumber = rand.nextInt(upperBound - lowerBound + 1) + lowerBound;
-        return randomNumber;
+    int randomNumber = rand.nextInt(upperBound - lowerBound + 1) + lowerBound;
+    return randomNumber;
 
+  }
+
+  public static int rollDicesSum(int times){
+    int result=0;
+    for(int i=0; i<times; i++){
+      result += RandomGenerator.generateNumberBetween(1,6);
     }
+    return result;
+  }
 
 }
