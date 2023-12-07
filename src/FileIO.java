14d8d20e34eb985dbaa75f758c2a3e01be2dbff4
lauhoyin3a <62736148+lauhoyin3a@users.noreply.github.com>
2023-12-07 11:47:49 +0800

    Update

--- src/FileIO.java
+++ src/FileIO.java
@@ -1,43 +1,50 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class FileIO {
 
-    private final static String fileName = "boosts.txt";
+  private final static String boostFileName = "boosts.txt";
+  private final static String outcomeFileName = "outcome.txt";
 
-    public FileIO() {
+  public FileIO() {
 
+  }
+
+  public String getBoostFileName() {
+    return boostFileName;
+  }
+
+  public String getOutComeFileName(){
+    return outcomeFileName;
+  }
+
+  public String readFileContents() throws FileNotFoundException {
+    StringBuilder fileContent = new StringBuilder();
+    try (FileReader reader = new FileReader(boostFileName);
+        Scanner fileInput = new Scanner(reader)) {
+
+      while (fileInput.hasNextLine()) {
+        fileContent.append(fileInput.nextLine()).append("\n");
+      }
+    } catch (FileNotFoundException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException("Error in reading from file! Exiting...", e);
     }
+    return fileContent.toString();
+  }
 
-    public String readFileContents() throws FileNotFoundException {
-        StringBuilder fileContent = new StringBuilder();
-        try (FileReader reader = new FileReader(fileName);
-             Scanner fileInput = new Scanner(reader)) {
-
-            while (fileInput.hasNextLine()) {
-                fileContent.append(fileInput.nextLine()).append("\n");
-            }
-        } catch (FileNotFoundException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new RuntimeException("Error in reading from file! Exiting...", e);
-        }
-        return fileContent.toString();
+  public void writeToFile(String content) throws IOException {
+    try (FileWriter writer = new FileWriter(outcomeFileName)) {
+      writer.write(content);
+    } catch (IOException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new RuntimeException("Error in writing to file! Exiting...", e);
     }
+  }
 
-    public String getFileName() {
-        return fileName;
-    }
-
-    public static void main(String[] args) {
-        FileIO fileReader = new FileIO();
-        try {
-            System.out.println(fileReader.readFileContents());
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + fileReader.getFileName());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
