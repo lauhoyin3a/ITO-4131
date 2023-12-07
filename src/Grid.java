d3f7c2f3de51aa750a1af9b7d4f7b8dd2458ebe0
lauhoyin3a <62736148+lauhoyin3a@users.noreply.github.com>
2023-12-07 11:48:04 +0800

    Update

--- src/Grid.java
+++ src/Grid.java
@@ -1,28 +1,133 @@
+
 public class Grid {
-    private int damage;
-    private int defence;
-    private int coins;
-    private int xCoordinate;
-    private int yCoordinate;
-    private Player owner;
 
-    public Grid(int damage, int defence, int coins, int xCoordinate, int yCoordinate) {
-        this.damage = damage;
-        this.defence = defence;
-        this.coins = coins;
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
-    }
+  private int coins;
+  private int damage;
+  private Player owner;
+  private int defence;
+  private int xCoordinate;
+  private int yCoordinate;
 
-    @Override
-    public String toString() {
-        return "Grid{" +
-                "damage=" + damage +
-                ", defence=" + defence +
-                ", coins=" + coins +
-                ", xCoordinate=" + xCoordinate +
-                ", yCoordinate=" + yCoordinate +
-                ", owner=" + owner +
-                '}';
+  public Grid() {
+
+  }
+
+  public Grid(int damage, int defence, int coins, int xCoordinate, int yCoordinate) {
+    this.damage = damage;
+    this.defence = defence;
+    this.coins = coins;
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+  }
+
+  public Grid(int damage, int defence, int coins, int xCoordinate, int yCoordinate, Player owner) {
+    this.damage = damage;
+    this.defence = defence;
+    this.coins = coins;
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+    this.owner = owner;
+  }
+
+  public int getCoins() {
+    return coins;
+  }
+
+  public int getDamage() {
+    return damage;
+  }
+    public int getDefence() {
+    return defence;
+  }
+
+  public Player getOwner() {
+    return owner;
+  }
+
+  public int getxCoordinate() {
+    return xCoordinate;
+  }
+
+  public int getyCoordinate() {
+    return yCoordinate;
+  }
+
+
+  public boolean isOwnByComputer(){
+    try{
+    if (this.getOwner() instanceof Computer){
+      return true;
     }
+    }
+    catch(NullPointerException e){
+      return false;
+    }
+    return false;
+  }
+
+  public boolean isOwnByHuman(){
+    try{
+    if (this.getOwner() instanceof Human){
+      return true;
+    }
+    }
+    catch(NullPointerException e){
+      return false;
+    }
+    return false;
+  }
+
+  public void setCoins(int coins) {
+    this.coins = coins;
+  }
+
+  public void setDamage(int damage) {
+    this.damage = damage;
+  }
+
+
+  public void setDefence(int defence) {
+    this.defence = defence;
+  }
+
+  public void setOwner(Player owner) {
+    this.owner = owner;
+  }
+
+  public void setxCoordinate(int xCoordinate) {
+    this.xCoordinate = xCoordinate;
+  }
+
+  public void setyCoordinate(int yCoordinate) {
+    this.yCoordinate = yCoordinate;
+  }
+
+
+  public String toString() {
+    String owner;
+    try{
+      boolean ownByComputer = this.getOwner() instanceof Computer;
+      boolean ownByHuman = this.getOwner() instanceof Human;
+      if (ownByComputer == true){
+        owner = "C";
+      }
+      else if (ownByHuman == true){
+        owner = "P";
+      }
+      else{
+        owner = " ";
+      }
+    }
+    catch(NullPointerException e){
+      owner=" ";
+    }
+    String damage = String.format("%2d",this.damage);
+    String coins = String.format("%5d", this.coins);
+
+    return "(" + damage +
+        "," + defence +
+        "," + coins +
+        "," + owner +
+        ')';
+  }
 }
